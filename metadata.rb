de9278972f0d8400502ba653ef9bf5e804f0d4e7
Nick Dobson <nick.dobson@geocent.com>
2012-08-16 19:42:23 -0500

    metadata for weblogic cookbook, vagrant doesn't like it not to be there…

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Geocent"
+maintainer_email "noemail@geocent.com"
+license          "All rights reserved"
+description      "Installs/Configures Weblogic"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
